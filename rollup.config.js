8763eea5404470447790e5c55a5d9fdbaeafabab
Andreas Rozek <A.Rozek@gmx.de>
2023-11-25 07:10:22 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -6,15 +6,19 @@ export default {
   input: './src/node-red-javascript-interface-library.js',
   output: [
     {
-      file:     './dist/node-red-javascript-interface-library.js',
+      file:     './dist/node-red-javascript-interface-library.umd.js',
       format:    'umd', // builds for both Node.js and Browser
       name:      'JIL', // required for UMD modules
-      noConflict:false,
       sourcemap: true,
       plugins: [terser({ format:{ comments:false, safari10:true } })],
     },{
       file:     './dist/node-red-javascript-interface-library.esm.js',
       format:   'esm',
+      sourcemap:true,
+    },{
+      file:     './dist/node-red-javascript-interface-library.iife.js',
+      format:   'iife',
+      name:     'JIL',
       sourcemap:true,
     }
   ],
