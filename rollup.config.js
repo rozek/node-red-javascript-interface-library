0325c0b3695089874bf46ae00e034b07fcf6c2c1
Andreas Rozek <A.Rozek@gmx.de>
2023-11-25 07:46:59 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 // see https://github.com/rozek/build-configuration-study
 
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: './src/node-red-javascript-interface-library.js',
+  input: './src/node-red-javascript-interface-library.ts',
   output: [
     {
       file:     './dist/node-red-javascript-interface-library.umd.js',
@@ -20,6 +21,10 @@ export default {
       format:   'iife',
       name:     'JIL',
       sourcemap:true,
+      plugins: [terser({ format:{ comments:false, safari10:true } })],
     }
+  ],
+  plugins: [
+    typescript(),
   ],
 };
